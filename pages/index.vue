f81341583908aa0b0b94813f15fe62d9a990905b
hodory <mo02@outlook.kr>
2019-06-22 02:59:54 +0900

    feat : 가능한 시간대를 전부 배열에 담아 겹치는 시간 항목 제거

--- pages/index.vue
+++ pages/index.vue
@@ -8,58 +8,99 @@
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-import TimeTable from "../components/TimeTable";
-import Major from "../components/Major";
+  import AppLogo from '~/components/AppLogo.vue'
+  import TimeTable from "../components/TimeTable";
+  import Major from "../components/Major";
+  import _ from "lodash";
+  import Moment from 'moment';
+  import {extendMoment} from 'moment-range';
 
-export default {
-  components: {
-    Major,
-    TimeTable,
-    AppLogo
-  },
-  data() {
-    return {
-      cases: []
-    }
-  },
-  methods: {
-    // 조회하기 버튼 선택시 처리하는 부분
-    handleCases(selectedSubject) {
-      this._makeCases(selectedSubject);
+  const moment = extendMoment(Moment);
+
+  export default {
+    components: {
+      Major,
+      TimeTable,
+      AppLogo
     },
-    _makeCases(subjectList) {
-      // TODO : subjectList를 받아 처리하는 부분 작성
+    data() {
+      return {
+        cases: []
+      }
+    },
+    methods: {
+      // 조회하기 버튼 선택시 처리하는 부분
+      handleCases(selectedSubject) {
+        const optionList = _.map(selectedSubject, 'options');
+        // 시간 객체를 모두 담음.
+        let cases = this._makeCases(optionList);
+        // TODO : 타이틀을 가져올 수 있도록 변경 필요
+        // 중복 제거
+        _.forEach(cases, (_case, _key) => {
+          const times = _.mapValues(_case, 'times');
+          let isOverlapped = false;
+          let diffOverlap = [];
+          _.forEach(times, (v1) => {
+            _.forEach(v1, (v2) => {
+              const startTime = v2.startTime.split(":");
+              const endTime = v2.endTime.split(":");
+              const sMoment = moment().days(v2.day).hour(startTime[0]);
+              const eMoment = moment().days(v2.day).hour(endTime[0]);
+              const range = moment.range(sMoment, eMoment);
+              if (diffOverlap) {
+                _.forEach(diffOverlap, (value) => {
+                  if (range.overlaps(value)) {
+                    isOverlapped = true;
+                    return false;
+                  }
+                });
+              }
+              if (isOverlapped) {
+                console.log(`isOverlap first | day : ${v2.day} |startTime : ${v2.startTime} | endTime : ${v2.endTime}`);
+                return false;
+              } else {
+                diffOverlap.push(range);
+              }
+            });
+            if (isOverlapped) {
+              console.log(`isOverlap second`);
+              return false;
+            }
+          });
+          if (isOverlapped) {
+            console.log(`isOverlap remove`);
+            cases.splice(_key, 1);
+          }
+        });
+
+        this.cases = cases;
+      },
+      // 가능한 경우를 만들어주는 함수
+      _makeCases(arg) {
+        let r = [], max = arg.length - 1;
+
+        function helper(arr, i) {
+          for (let j = 0, l = arg[i].length; j < l; j++) {
+            let a = arr.slice(0);
+            a.push(arg[i][j]);
+            if (i === max) {
+              r.push(a);
+            } else
+              helper(a, i + 1);
+          }
+        }
+
+        helper([], 0);
+        return r;
+      },
     }
   }
-}
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .container {
+    min-height: 100vh;
+    text-align: center;
+  }
 </style>
 
